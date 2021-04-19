2b8d2d9ed86cd530de901fc13927bce0c4a3abc1
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-19 23:11:31 +0200

    [INIT] Initiated a ASP DotNet Core 3.1 Project with ASP NET MVC arch

---
+++ FripShop/UnitTestFripShop/UserTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFripShop
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
