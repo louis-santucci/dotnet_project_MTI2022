55e891af08db39fa9f71db9d58965c2358525d01
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-20 00:43:56 +0200

    [Init][WIP] Added FripShop Context file

---
+++ FripShop/FripShop/Models/FripShopContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FripShop.Models
+{
+    public class BloggingContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-4MS830T\\MTI;Initial Catalog=FripShop;Trusted_Connection=True");
+        }
+    }
+}
