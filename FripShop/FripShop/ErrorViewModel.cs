635e1fa54f6ef687c2ee13d4e6ba2923a59195ab
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-29 19:03:55 +0200

    Added ErrorViewModel

---
+++ FripShop/FripShop/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FripShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
