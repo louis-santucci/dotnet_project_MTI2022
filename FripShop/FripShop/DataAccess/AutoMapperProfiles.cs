e562a5272fb2b2507be8940c1441d96192dcc19f
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-29 19:04:15 +0200

    Added AutoMapperProfile for all models

---
+++ FripShop/FripShop/DataAccess/AutoMapperProfiles.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using FripShop.Dbo;
+using FripShop.DataAccess.EFModels;
+using FripShop.Models.EfModels;
+
+namespace FripShop.DataAccess
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DboArticle, EFModels.Article>();
+            CreateMap<EFModels.Article, DboArticle>();
+
+            CreateMap<DboCart, EFModels.Cart>();
+            CreateMap<EFModels.Cart, DboCart>();
+
+            CreateMap<DboRating, EFModels.Rating>();
+            CreateMap<EFModels.Rating, DboRating>();
+
+            CreateMap<DboTransaction, EFModels.Transaction>();
+            CreateMap<EFModels.Transaction, DboTransaction>();
+
+            CreateMap<DboUser, EFModels.User>();
+            CreateMap<EFModels.User, DboUser>();
+        }
+    }
+}
