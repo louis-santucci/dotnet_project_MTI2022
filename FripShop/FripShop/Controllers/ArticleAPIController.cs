eb4aa5e310d57be1a8a0669a09cc105f8508f388
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-30 14:24:25 +0200

    [Controller] Adding temp file for api

---
+++ FripShop/FripShop/Controllers/ArticleAPIController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FripShop.DataAccess;
+using FripShop.DataAccess.EFModels;
+using FripShop.DataAccess.Interfaces;
+using FripShop.Dbo;
+using Microsoft.Extensions.Logging;
+
+namespace FripShop.Controllers
+{
+    [Route("api/articles")]
+    [ApiController]
+    public class ArticleAPIController : ControllerBase
+    {
+
+        private readonly IArticleRepo _articleRepo;
+        private readonly IUserRepo _userRepo;
+
+        public ArticleAPIController(IArticleRepo articleRepo, IUserRepo userRepo)
+        {
+            _articleRepo = articleRepo;
+            _userRepo = userRepo;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult> GetAll()
+        {
+            var results = await this._articleRepo.Get();
+            return Ok(results);
+        }
+
+        [HttpGet("{articleId}")]
+        public async Task<ActionResult> GetId(long articleId)
+        {
+            var articles = await this._articleRepo.Get();
+            if (articles.Any(c => c.Id == articleId))
+                return Ok(articles.Single(c => c.Id == articleId));
+            return NotFound();
+        }
+
+        [HttpGet("{articleId}/getUser")]
+        public async Task<ActionResult> GetUserFromArticle(long articleId)
+        {
+            var user = await _articleRepo.GetUserFromId(articleId);
+            if (user != null)
+                return Ok(user);
+            return NotFound();
+        }
+    }
+}
