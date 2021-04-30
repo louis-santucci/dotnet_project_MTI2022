4d7b704a0539810206e7396ce63a5d358e6323a7
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-30 19:02:13 +0200

    [Controller] Updated controllers

--- FripShop/FripShop/Controllers/HomeController.cs
+++ FripShop/FripShop/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using FripShop.Dbo;
+using FripShop.DTO;
 
 namespace FripShop.Controllers
 {
--- FripShop/FripShop/Controllers/UserController.cs
+++ FripShop/FripShop/Controllers/UserController.cs
@@ -3,85 +3,132 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using FripShop.DataAccess.EFModels;
+using FripShop.DataAccess.Interfaces;
+using FripShop.DTO;
+using Microsoft.Extensions.Logging;
 
 namespace FripShop.Controllers
 {
-    public class AccountController : Controller
+    public class UserController : Controller
     {
-        // GET: AccountController
-        public ActionResult Index()
+        private readonly IArticleRepo _articleRepo;
+        private readonly IUserRepo _userRepo;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger, IArticleRepo articleRepo, IUserRepo userRepo)
         {
-            return View();
+            this._userRepo = userRepo;
+            this._articleRepo = articleRepo;
+            this._logger = logger;
         }
 
-        // GET: AccountController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: AccountController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: AccountController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Register([FromBody] DTOUser userModel)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    if (_userRepo.GetUserByEmail(userModel.Email) != null)
+                        return BadRequest(userModel);
+                    if (_userRepo.GetUserByUserName(userModel.UserName) != null)
+                        return BadRequest(userModel);
+                    var result = await _userRepo.Insert(userModel);
+                    if (result != null)
+                        return Created(result.Id.ToString(), result);
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                _logger.LogError("CONTROLLER USER -- Register() -- Error on db : ", ex);
+                return BadRequest();
             }
+            return BadRequest();
         }
 
-        // GET: AccountController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: AccountController/Edit/5
-        [HttpPost]
+        [HttpGet]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Get(int id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var user = await _userRepo.GetById(id);
+                if (user != null)
+                    return Ok(User);
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                _logger.LogError("CONTROLLER USER -- Get() -- Error on db : ", ex);
             }
+            return NotFound();
         }
 
-        // GET: AccountController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: AccountController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit([FromBody] DTOUser userModel)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    var currentUser = await _userRepo.GetById(userModel.Id);
+                    if (currentUser == null)
+                        return NotFound();
+                    currentUser.Gender = userModel.Gender;
+                    currentUser.Name = userModel.Name;
+                    currentUser.UserName = userModel.UserName;
+                    currentUser.Password = userModel.Password;
+                    currentUser.Address = userModel.Address;
+                    currentUser.Email = userModel.Email;
+
+                    if (_userRepo.GetUserByEmail(currentUser.Email) != null
+                        && _userRepo.GetUserByUserName(currentUser.UserName) != null)
+                    {
+                        var result = await _userRepo.Update(currentUser);
+                        if (result != null)
+                            return Ok(result);
+                    }
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                _logger.LogError("CONTROLLER USER -- Edit() -- Error on db : ", ex);
+                return BadRequest();
             }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete([FromBody] long id)
+        {
+            try
+            {
+                if (await _userRepo.Delete(id) == false)
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("CONTROLLER USER -- Delete() -- Error on db : ", ex);
+                return BadRequest();
+            }
+
+            return Ok(id);
         }
     }
 }
