7485f85ef870301ea6ca1aebab82ad1bf0624d54
Antoine du FOU <antoine.du-fou@epita.fr>
2021-04-30 17:47:31 +0200

    [CONNECTION STRING] Antoine connection string in startup

--- FripShop/FripShop/Startup.cs
+++ FripShop/FripShop/Startup.cs
@@ -34,7 +34,7 @@ namespace FripShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            _connectionString = Configuration.GetConnectionString("FripShop");
+            _connectionString = Configuration.GetConnectionString("Antoine");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
