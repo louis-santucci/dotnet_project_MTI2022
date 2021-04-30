d34c6bb01ccc340462b06ca3466506d9969061cd
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-30 19:02:48 +0200

    [DTO] Added class for minimal information for articles etc

---
+++ FripShop/FripShop/DTO/DTOUserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace FripShop.DTO
+{
+    public class DTOUserInfo : DTOUser, IDTO
+    {
+        [Key]
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Note { get; set; }
+    }
+}
