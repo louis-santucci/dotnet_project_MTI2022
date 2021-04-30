c6459857ef0d47f45067907b8f75a8d9d8cd3713
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-30 17:29:11 +0200

    [Controller] Adding prototype for func

--- FripShop/FripShop/Controllers/ArticleController.cs
+++ FripShop/FripShop/Controllers/ArticleController.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Threading.Tasks;
 using FripShop.DataAccess.Interfaces;
+using FripShop.Dbo;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
 
 namespace FripShop.Controllers
@@ -49,6 +51,12 @@ namespace FripShop.Controllers
             if (user != null)
                 return Ok(user);
             return NotFound();
+        }
+
+        [HttpPost("/api/articles/create")]
+        public async Task<ActionResult> CreateArticle([FromBody] DboArticle article)
+        {
+
         }
     }
 }
