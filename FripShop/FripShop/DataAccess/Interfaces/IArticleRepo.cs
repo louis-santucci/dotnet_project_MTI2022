531edf08debb55cca4113a25dd6b4104f81fd986
Louis SANTUCCI <louis.santucci@epita.fr>
2021-04-30 03:01:49 +0200

    [Interfaces] Added interfaces for user and articles

---
+++ FripShop/FripShop/DataAccess/Interfaces/IArticleRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using FripShop.DataAccess.EFModels;
+using FripShop.Dbo;
+
+namespace FripShop.DataAccess.Interfaces
+{
+    public interface IArticleRepo : IRepo<Article, DboArticle>
+    {
+        public Task<DboUser> GetUserFromId(long id);
+    }
+}
---
+++ FripShop/FripShop/DataAccess/Interfaces/IUserRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FripShop.DataAccess.EFModels;
+using FripShop.Dbo;
+
+namespace FripShop.DataAccess.Interfaces
+{
+    public interface IUserRepo : IRepo<User, DboUser> { }
+}
