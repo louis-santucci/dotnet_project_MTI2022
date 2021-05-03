70a7fec7f8f8f7ff13df2d11b66faebb0a83acd5
Antoine du FOU <antoine.du-fou@epita.fr>
2021-05-03 19:27:16 +0200

    [USER] Removed duplicate HashPassword function in Controller

--- FripShop/FripShop/Controllers/UserController.cs
+++ FripShop/FripShop/Controllers/UserController.cs
@@ -183,23 +183,6 @@ namespace FripShop.Controllers
             return BadRequest();
         }*/
 
-        private string HashPassword(string password)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-
-            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
-
-            byte[] result = md5.Hash;
-
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < result.Length; i++)
-            {
-                strBuilder.Append(result[i].ToString("x2"));
-            }
-
-            return strBuilder.ToString();
-        }
-
         /// API Calls
         [HttpPost("/api/users/login")]
         [ValidateAntiForgeryToken]
