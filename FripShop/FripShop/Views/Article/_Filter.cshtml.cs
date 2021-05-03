f4368e929b2c80b13c68435a9a56d64d0f592add
Antoine du FOU <antoine.du-fou@epita.fr>
2021-05-03 20:42:57 +0200

    [FILTER] Added filter for gender

---
+++ FripShop/FripShop/Views/Article/_Filter.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FripShop.Views.Article
+{
+    public class _Filter
+    {   
+          public string gender { get; set; }
+    }
+}
