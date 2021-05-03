d80bf45bb93de58c96e24f6cded353ec81965ed1
Antoine du FOU <antoine.du-fou@epita.fr>
2021-05-04 00:49:01 +0200

    [FILTER] Added category, and made the style of the filter form. May have broken some filters.

--- FripShop/FripShop/Views/Article/_Filter.cshtml.cs
+++ FripShop/FripShop/Views/Article/_Filter.cshtml.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 namespace FripShop.Views.Article
 {
     public class _Filter
-    {   
-          public string gender { get; set; }
-          public List<string> categories { get; set; }
-          public string minPrice { get; set; }
-          public string maxPrice { get; set; }
-          public string conditionMin { get; set; }
-          public string sortBy { get; set; }
-          public string ascending { get; set; }
-          public string search { get; set; }
+    {
+        public enum Category
+        {
+            Hauts,
+            Bas,
+            Chaussures,
+            Accessoires
+        }
+        public enum Comparison
+        {
+            Date,
+            Prix,
+            Condition,
+            Note
+        }
+
+        public string gender { get; set; }
+        public string category { get; set; }
+        public string minPrice { get; set; }
+        public string maxPrice { get; set; }
+        public string conditionMin { get; set; }
+        public string sortBy { get; set; }
+        public string ascending { get; set; }
+        public string search { get; set; }
     }
 }
