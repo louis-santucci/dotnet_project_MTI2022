011ecf2991858e9e3f8e17b4c7625111216cf436
Louis SANTUCCI <louis.santucci@epita.fr>
2021-05-06 00:06:14 +0200

    [UNitTestCartRepo] Added file

---
+++ FripShop/UnitTestFripShop/CartTests/CartRepoTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestFripShop.CartTests
+{
+    /// <summary>
+    /// Claxs for article CRUD unit testing
+    /// </summary>
+    /// 
+    public class CartRepoTests
+    {
+    }
+}
