a6f4572014a09ed78115a56f61ff40ff9264616b
Antoine du FOU <antoine.du-fou@epita.fr>
2021-05-06 00:12:17 +0200

    [TEST CONTROLLER USER] Ready to begin

--- FripShop/UnitTestFripShop/UserTests/UserControllerTests.cs
+++ FripShop/UnitTestFripShop/UserTests/UserControllerTests.cs
@@ -6,19 +6,23 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnitTestFripShop.ArticleTests;
+using UnitTestFripShop.CartTests;
 
 namespace UnitTestFripShop.UserTests
 {
     [TestClass]
     public class UserControllerTests
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<UserController> _logger;
 
         [TestMethod]
         public void RegisterFirstValidUser()
         { 
             var userMockRepo = new UserMockRepo()._mockRepo;
-            //var userController = new UserController(_logger, userMockRepo, cartMockRepo);
+            var articleMockRepo = new ArticleMockRepo()._articleRepo;
+            var cartMockRepo = new CartMockRepo()._mockRepo;
+            var userController = new UserController(_logger, articleMockRepo, userMockRepo, cartMockRepo);
         }
     }
 }
