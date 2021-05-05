5a8a9f52938e9aeb52d70befb4b5134711867031
Antoine du FOU <antoine.du-fou@epita.fr>
2021-05-05 19:55:22 +0200

    [TEST CONTROLLER USER] Init of User Controller Tests - need for Cart Mock Repo to continue

---
+++ FripShop/UnitTestFripShop/UserTests/UserControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using FripShop.Controllers;
+using FripShop.DataAccess.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestFripShop.UserTests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        private readonly ILogger _logger;
+
+        [TestMethod]
+        public void RegisterFirstValidUser()
+        { 
+            var userMockRepo = new UserMockRepo()._mockRepo;
+            //var userController = new UserController(_logger, userMockRepo);
+        }
+    }
+}
