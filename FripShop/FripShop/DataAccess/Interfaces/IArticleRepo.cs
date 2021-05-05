d76260c83e90b143e79110c0a0a67d3017b22bcc
Louis SANTUCCI <louis.santucci@epita.fr>
2021-05-05 02:30:19 +0200

    [COdeDecoration] Adding commentaries

--- FripShop/FripShop/DataAccess/Interfaces/IArticleRepo.cs
+++ FripShop/FripShop/DataAccess/Interfaces/IArticleRepo.cs
@@ -10,12 +10,17 @@ namespace FripShop.DataAccess.Interfaces
     public interface IArticleRepo : IRepo<Article, DTOArticle>
     {
         /// <summary>
-        /// Get user in function of its ID
+        /// Get DTO user in function of its ID
         /// </summary>
-        /// <param name="id">the ID of the wanted user</param>
-        /// <returns>The wanted user with all its information</returns>
+        /// <param name="id">the ID of the wanted DTO user</param>
+        /// <returns>The wanted DTO user with all its information</returns>
         public DTOUser GetUserFromId(long id);
 
+        /// <summary>
+        /// Gets DTO article in function of its ID
+        /// </summary>
+        /// <param name="articleId">The ID of the wanted DTO article</param>
+        /// <returns></returns>
         public Task<DTOArticle> GetArticleFromId(long articleId);
     }
 }
